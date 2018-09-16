351efd64bfa3de71ca22656e75f1012858c53cdb
Luis Güette <guetteluis@gmail.com>
2018-09-16 15:53:02 -0400

    first test

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,28 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    PID::Kp = Kp;
+    PID::Ki = Ki;
+    PID::Kd = Kd;
+
+    p_error = 0.0;
+    i_error = 0.0;
+    d_error = 0.0;
 }
 
 void PID::UpdateError(double cte) {
+
+    // Integral error.
+    i_error += cte;
+
+    // Diferential error.
+    d_error = cte - p_error;
+    
+    // Proportional error.
+    p_error = cte;
 }
 
 double PID::TotalError() {
+    return p_error * Kp + i_error * Ki + d_error * Kd;
 }
 
